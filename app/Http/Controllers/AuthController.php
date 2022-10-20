332b659fdb132f4b0c244d4db0c913c536ad3166
YoussefLMT <mcyoussef324@gmail.com>
2022-10-20 20:13:08 +0100

    add register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    
+    public function register(Request $req){
+        
+        $validator = Validator::make($req->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            $user = User::create([
+                'name' => $req->name,
+                'email' => $req->email,
+                'password' => Hash::make($req->password),
+            ]);
+
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Your account created successfully',
+            ]);
+            
+        }
+    }
 }
