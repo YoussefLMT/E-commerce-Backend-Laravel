5f14360c11fed50d4aef217c0c867f45dd1c48ab
YoussefLMT <mcyoussef324@gmail.com>
2022-10-21 18:29:42 +0100

    add addProduct function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,48 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    
+    public function addProduct(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+            'price'=> 'required',
+            'quantity' => 'required',
+            'category' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg',
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            $product = new Product;
+            $product->name = $request->name;
+            $product->price = $request->price;
+            $product->quantity = $request->quantity;
+            $product->category = $request->category;
+            $product->description = $request->description;
+
+            if($request->hasFile('image'))
+            {
+                $image = $request->file('image');
+                $extension = $image->getClientOriginalExtension();
+                $image_name = time(). '.' .$extension;
+                $image->move('uploads/images', $image_name);
+                $product->image = 'uploads/images/' . $image_name;
+            }
+
+            $product->save();
+    
+            return response()->json([
+                'status' => 200,
+                'message' => "Product added successfully",
+            ]);
+        }
+    }
 }
