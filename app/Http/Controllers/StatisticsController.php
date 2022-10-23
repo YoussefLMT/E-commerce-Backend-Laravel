30b0608eb44c0609daf71f8f56296a30c6bc2aaa
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 21:04:20 +0100

    add getTotalCount function

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class StatisticsController extends Controller
 {
-    //
+    
+    public function getTotalCount(){
+
+        $productsCount = Product::all()->count();
+        $usersCount = User::all()->count();
+        $ordersCount = Order::all()->count();
+        $income = DB::table('orders')->sum('orders.total_amount');
+
+        return response()->json([
+            'status' => 200,
+            'productsCount' => $productsCount,
+            'usersCount' => $usersCount,
+            'ordersCount' => $ordersCount,
+            'income' => $income
+        ]);
+    }
 }
