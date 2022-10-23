0925c7e17ded499f1ad609258f16e6e25d93d4a6
YoussefLMT <mcyoussef324@gmail.com>
2022-10-23 12:32:16 +0100

    add addUser function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+   
+    public function addUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            User::create([
+                'name'=> $request->name,
+                'email'=> $request->email,
+                'password' => Hash::make($request->password),
+                'role' => $request->role
+            ]);
+    
+            return response()->json([
+                'status' => 200,
+                'message' => "User added successfully",
+            ]);
+        }
+    }
+
 }
